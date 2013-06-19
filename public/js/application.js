83f30ce573745222b27de0c28f3864abd9d70255
Apprentice <apprentice@dbc09.local>
2013-06-18 19:22:42 -0700

    logout works!

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#sign-out').on('click', function(e) {
+    e.preventDefault();
+    $(this).append('<p>Signing you out...</p>');
+    $(this).append('<iframe id="logoutframe" src="https://accounts.google.com/logout" style="display: none"></iframe>');
+    setTimeout(function() { window.location = window.location.origin; }, 2000);
+  });
 });
